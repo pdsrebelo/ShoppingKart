1159e71dbece7d542e00cced3ef3f9517dc5cf16
Pedro Rebelo <p3dro_rebelo@hotmail.com>
2017-11-09 18:38:53 +0000

    Added ShoppingCart class.

---
+++ AdgisticsShoppingKart/AdgisticsShoppingKart.Model/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdgisticsShoppingKart.Model
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        public IEnumerable<Item> Items { get; set; }
+
+        public decimal Total(IEnumerable<Offer> currentOffers)
+        {
+            if (Items == null || !Items.Any())
+                return 0;
+
+            decimal total = 0;
+
+            foreach (var item in Items)
+            {
+                // Check if there's a promotion for this item
+
+            }
+
+            return total;
+        }
+    }
+}
